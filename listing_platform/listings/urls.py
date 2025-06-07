19deb70a377bed68c07ea89ed6f0baeac980173d
xzalerr <zalecki.raf@gmail.com>
2025-06-07 17:15:56 +0200

    Listing edition and delition added.

--- listing_platform/listings/urls.py
+++ listing_platform/listings/urls.py
@@ -6,5 +6,7 @@ app_name = 'listings'
 urlpatterns = [
     path('', views.browse_listings, name='list'),
     path('listing-new/', views.listing_new, name='listing-new'),
-    path('<slug:slug>', views.listing_page, name='page')
+    path('<slug:slug>', views.listing_page, name='page'),
+    path('listing-edit/<slug:slug>/', views.listing_edit, name='listing-edit'),
+    path('listing-delete/<slug:slug>/', views.listing_delete, name='listing-delete')
 ]
--- listing_platform/listings/views.py
+++ listing_platform/listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Listing
 from django.contrib.auth.decorators import login_required
 from . import forms
@@ -10,7 +10,8 @@ def browse_listings(request):
 
 def listing_page(request, slug):
     listing = Listing.objects.get(slug=slug)
-    return render(request, 'listings/listing_page.html', {'listing' : listing})
+    is_author = request.user.is_authenticated and listing.author == request.user
+    return render(request, 'listings/listing_page.html', {'listing' : listing, 'is_author': is_author})
 
 @login_required(login_url='/users/login/')
 def listing_new(request):
@@ -24,3 +25,29 @@ def listing_new(request):
     else:
         form = forms.CreateListing()
     return render(request, 'listings/listing_new.html', { 'form': form})
+
+@login_required(login_url='/users/login/')
+def listing_edit(request, slug):
+    listing = get_object_or_404(Listing, slug=slug)
+    if listing.author != request.user:
+        return redirect('listings:list')  # Redirect if the user is not the author
+
+    if request.method == 'POST':
+        form = forms.CreateListing(request.POST, request.FILES, instance=listing)
+        if form.is_valid():
+            form.save()
+            return redirect('listings:list')
+    else:
+        form = forms.CreateListing(instance=listing)
+    return render(request, 'listings/listing_edit.html', {'form': form, 'listing': listing})
+
+@login_required(login_url='/users/login/')
+def listing_delete(request, slug):
+    listing = get_object_or_404(Listing, slug=slug)
+    if listing.author != request.user:
+        return redirect('listings:list')  # Redirect if the user is not the author
+
+    if request.method == 'POST':
+        listing.delete()
+        return redirect('listings:list')
+    return render(request, 'listings/listing_delete.html', {'listing': listing})
